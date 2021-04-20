e656861401f347b33a7c0d6abbf6aaa88f0de52e
= <=>
2021-04-20 23:20:00 +0200

    Added tasklist model

---
+++ todolist.api/todolist.api/Controllers/TaskListController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using todolist.api.Model;
+
+namespace todolist.api.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class TaskListController
+    {
+        // POST: api/Task
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] EmptyTodoTaskList taskList)
+        {
+            await _taskQueries.Create(newTask);
+            _logger.LogInformation("Adding task {task}", JsonConvert.SerializeObject(newTask));
+            return CreatedAtAction("Get", new { id = newTask.Id });
+        }        
+    }
+}
---
+++ todolist.api/todolist.api/Model/EmptyTodoTaskList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace todolist.api.Model
+{
+    [Table ("TaskLists")]
+    public class EmptyTodoTaskList
+    {
+        public string Name { get; set; }
+        [ExplicitKey]
+        public Guid Id { get; set; }        
+    }
+}
